4aacf27017be5752e8131a7d1a17b72da0d15d91
Nicholas Jaquez <nrjaquez@gmail.com>
2021-01-03 22:26:12 -0800

    page setup 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-// import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./component/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./component/pages/Home";
 
 function App() {
-return 'Hello World!'
+  return (
+      <>
+      <Router>
+          <Navbar>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                {/* <Route path="/projects" exact component={Projects}/>
+                <Route path="/about" exact component={About}/>
+                <Route path="/contact" exact component={Contact}/> */}
+            </Switch>
+          </Navbar>
+      </Router>
+      </>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React>
-    <App />
-  </React>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React>
+//     <App />
+//   </React>,
+//   document.getElementById('root')
+// );
 
 
