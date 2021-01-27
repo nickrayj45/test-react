67fd922e9debfad8b3657901dbb0aac0486ba873
Nicholas Jaquez <nrjaquez@gmail.com>
2021-01-26 21:34:19 -0800

    trying to add about page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./component/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./component/pages/Home";
+import About from "./component/About";
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
                 {/* <Route path="/projects" exact component={Projects}/>
                 <Route path="/about" exact component={About}/>
                 <Route path="/contact" exact component={Contact}/> */}
+                <Route path="/about"/>
+                <About />
             </Switch>
           </Navbar>
       </Router>
--- src/component/Button.js
+++ src/component/Button.js
@@ -30,4 +30,6 @@ export const Button = ({
   );
 };
 
+export default Button;
+
 
